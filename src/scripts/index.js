b25d45d3dc46229aea5586265f5d78b49bbaa3c0
Stian Haklev <stian@minervaproject.com>
2021-01-27 17:16:27 +0100

    WIP

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,16 @@
 import RoamClient from "roam-client";
-import share from "sharedb";
 import json0 from "ot-json0";
+import ShareDBClient from "sharedb/lib/client";
 
+ShareDBClient.types.register(json0.type);
+ShareDBClient.types.defaultType = json0.type;
+
+const shareDbUrl = "wss://chilifrog.epfl.ch";
+
+const socket = new ReconnectingWebSocket(shareDbUrl, null, {
+  minConnectionDelay: 1
+});
+const connection = new ShareDBClient.Connection(socket);
 window.R = RoamClient;
+window.c = connection;
 console.log("Roam/Inter loaded");
