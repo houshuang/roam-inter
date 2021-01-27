4f798648ed9567aa0f6a25781e23bacde3f449b4
Stian Haklev <stian@minervaproject.com>
2021-01-27 17:02:30 +0100

    WIP

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,50 +1,53 @@
-const Path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: Path.resolve(__dirname, '../src/scripts/index.js'),
+    app: Path.resolve(__dirname, "../src/scripts/index.js")
   },
   output: {
-    path: Path.join(__dirname, '../build'),
-    filename: 'js/[name].js',
+    path: Path.join(__dirname, "../build"),
+    filename: "js/[name].js"
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
-      name: false,
-    },
+      chunks: "all",
+      name: false
+    }
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({ patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }] }),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html'),
+    new CopyWebpackPlugin({
+      patterns: [{ from: Path.resolve(__dirname, "../public"), to: "public" }]
     }),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, "../src/index.html")
+    })
   ],
   resolve: {
+    fallback: { stream: require.resolve("stream-browserify") },
     alias: {
-      '~': Path.resolve(__dirname, '../src'),
-    },
+      "~": Path.resolve(__dirname, "../src")
+    }
   },
   module: {
     rules: [
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: 'javascript/auto',
+        type: "javascript/auto"
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: '[path][name].[ext]',
-          },
-        },
-      },
-    ],
-  },
+            name: "[path][name].[ext]"
+          }
+        }
+      }
+    ]
+  }
 };
