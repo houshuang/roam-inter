037ad883153dcfcca4a830e9425e4824384e29ba
Stian Haklev <stian@minervaproject.com>
2021-01-27 16:57:23 +0100

    WIP

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,2 @@
-import "../styles/index.scss";
-
-if (process.env.NODE_ENV === "development") {
-  require("../index.html");
-}
-
-console.log("webpack starterkit");
+import RoamClient from "roam-client";
+window.R = RoamClient;
