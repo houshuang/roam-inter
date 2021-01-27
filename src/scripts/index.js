c52910abb34d68d53524ad05566966b8467a9c3b
Stian Haklev <stian@minervaproject.com>
2021-01-27 21:27:29 +0100

    WIP

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,71 @@
-import RoamClient from "roam-client";
-import json0 from "ot-json0";
-import ShareDBClient from "sharedb/lib/client";
-import ReconnectingWebsocket from "reconnecting-websocket";
+import json0 from "@minervaproject/ot-json0";
+import ShareDBClient from "@chilifrog/sharedb/lib/client";
+import ReconnectingWebSocket from "reconnecting-websocket";
+import { btree } from "./zettel";
 
-ShareDBClient.types.register(json0.type);
-ShareDBClient.types.defaultType = json0.type;
+const setup = () => {
+  ShareDBClient.types.register(json0.type);
+  ShareDBClient.types.defaultType = json0.type;
 
-const shareDbUrl = "wss://chilifrog.epfl.ch";
+  const shareDbUrl = "wss://icchilisrv3.epfl.ch/sharedb?null";
 
-const socket = new ReconnectingWebSocket(shareDbUrl, null, {
-  minConnectionDelay: 1
-});
-const connection = new ShareDBClient.Connection(socket);
-window.R = RoamClient;
-window.c = connection;
-console.log("Roam/Inter loaded");
+  const socket = new ReconnectingWebSocket(shareDbUrl, null, {
+    minConnectionDelay: 1
+  });
+  const connection = new ShareDBClient.Connection(socket);
+  global.c = connection;
+  global.rc = client;
+};
+
+// takes a tree of blocks, and sorts them recursively, using the order key
+const sortBlockTree = (btree, recur = 0) => {
+  if (btree) {
+    const res = btree.sort((x, y) => x.order - y.order);
+    const newRes = res.map(
+      x =>
+        x.children ? { ...x, children: sortBlockTree(x.children, recur++) } : x
+    );
+    return newRes;
+  }
+};
+
+// takes a sorted tree of blocks, and renders it as simple Markdown with hierarchical bullets
+const blocksToMarkdown = (blocks, indent = 0) => {
+  if (!blocks || blocks.length === 0) {
+    return "";
+  }
+  const md = blocks.reduce(
+    (acc, x) =>
+      `${acc}${"  ".repeat(indent * 2)}- ${x.string ||
+        x.title}\n${blocksToMarkdown(x.children, indent + 1)}`,
+    ""
+  );
+  return md;
+};
+
+// takes a block uid and returns a tree of blocks, sorted
+const getBlockWithChildren = uid => {
+  const blocks = roamAlphaAPI.q(
+    `[ :find (pull ?e [ :node/title :block/string :block/children :block/order {:block/children ...} ]) :where [?e :block/uid "${uid}"]]`
+  );
+  return sortBlockTree(blocks[0]);
+};
+
+// takes a title string and returns a tree of blocks, sorted
+const getPageWithChildren = title => {
+  const blocks = roamAlphaAPI.q(
+    `[ :find (pull ?e [ :node/title :block/string :block/children :block/order {:block/children ...} ]) :where [?e :node/title "${title}"]]`
+  );
+  return sortBlockTree(blocks[0]);
+};
+
+// setup();
+// tryStuff();
+//
+// const a = sortBlockTree(btree[0][0].children);
+// console.dir(a, { depth: null, colors: true });
+// console.log(blocksToMarkdown(a));
+global.inter = {};
+global.inter.blocksToMarkdown = blocksToMarkdown;
+global.inter.getPageWithChildren = getPageWithChildren;
+global.inter.getBlockWithChildren = getBlockWithChildren;
