884a3671cbec4e6b17b916c832a846efb6f7327b
Stian Haklev <stian@minervaproject.com>
2021-02-19 12:40:31 +0100

    WIP

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,3 @@
-import { btree, btreeBlock, btreeBlock2 } from "./zettel";
-import { btreeDiff, btreeToBArray, bArrayToBtree } from "./btreeDiff";
-import { setupSharedb, connection } from "./sharedb";
-import { setupInterval } from "./publications";
-import { setupConstants } from "./blockHelpers";
-import {
-  blocksToMarkdown,
-  getPageWithChildren,
-  getBlockWithChildren,
-  insertBlockTreeAsChild
-} from "./blockHelpers";
+import  {setup} from "./sharedb";
 
-if (!global.inter) {
-  global.inter = {};
-}
-
-global.inter.blocksToMarkdown = blocksToMarkdown;
-global.inter.getPageWithChildren = getPageWithChildren;
-global.inter.getBlockWithChildren = getBlockWithChildren;
-global.inter.insertBlockTreeAsChild = insertBlockTreeAsChild;
-global.inter.btreeDiff = btreeDiff;
-global.inter.btreeToBArray = btreeToBArray;
-global.inter.bArrayToBtree = bArrayToBtree;
-
-setupSharedb(() => {
-  setupConstants();
-  setupInterval();
-});
+setup()
--- src/scripts/sharedb.js
+++ src/scripts/sharedb.js
@@ -2,35 +2,56 @@ import ShareDBClient from "@chilifrog/sharedb/lib/client";
 import WebSocket from "reconnecting-websocket";
 import json0 from "@minervaproject/ot-json0";
 
-export const setupSharedb = callback => {
-  console.log("Setting up sharedb");
-  ShareDBClient.types.register(json0.type);
-  ShareDBClient.types.defaultType = json0.type;
+if (!global.inter) {
+  global.inter = {};
+}
 
-  const shareDbUrl = "wss://icchilisrv3.epfl.ch/sharedb?null";
+export const setup = () =>{
+    ShareDBClient.types.register(json0.type);
+    ShareDBClient.types.defaultType = json0.type;
 
-  const socket = new WebSocket(shareDbUrl);
-  const connection = new ShareDBClient.Connection(socket);
-  window.inter.sharedbDoc = connection.get("rz", "roam-inter4");
-  console.log(inter.sharedbDoc);
-  window.inter.sharedbDoc.subscribe();
-  window.inter.sharedbDoc.once("load", () => {
-    if (!window.inter.sharedbDoc.type) {
-      console.log("initiating doc");
-      window.inter.sharedbDoc.create({ changes: [] });
-    }
-    console.log("ready, callback");
-    callback();
+    const shareDbUrl = "wss://icchilisrv3.epfl.ch/sharedb?null";
+
+    const socket = new WebSocket(shareDbUrl);
+    global.inter.connection = new ShareDBClient.Connection(socket);
+ } 
+
+global.inter.getStream = (streamname, callback) =>
+  new Promise((resolve) => {
+    const doc = global.inter.connection.get("rz", streamname);
+    doc.subscribe();
+
+    doc.pushChange = (change) => {
+      if (!change)  {
+        return;
+      }
+      doc.submitOp({
+        p: ["changes", 99999],
+        li: change,
+      });
+
+    };
+
+    doc.rewrite = (change) => {
+      doc.submitOp({
+        p: ["changes"],
+        oi: change,
+      });
+    };
+
+    doc.getData = () => doc.data.changes;
+    console.log('callback',callback)
+
+    doc.on('op', (op)=>{
+      console.log('incoming op', op)
+      if(callback) { callback(doc.data.changes)}
+    })
+
+    doc.once("load", () => {
+      if (!doc.type) {
+        console.log("initiating doc");
+        doc.create({ changes: [] });
+      }
+      resolve(doc);
+    });
   });
-};
-
-export const pushChange = change => {
-  if (!change || !change.type) {
-    return;
-  }
-  console.log(change);
-  window.inter.sharedbDoc.submitOp({
-    p: ["changes", 99999],
-    li: change
-  });
-};
